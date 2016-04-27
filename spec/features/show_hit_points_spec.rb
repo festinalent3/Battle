68fcbe39cdedea059776a2ecdf6d9ce7a5d4101a
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-04-27 13:47:19 +0200

    Challenge 21: Test Helpers

--- spec/features/show_hit_points_spec.rb
+++ spec/features/show_hit_points_spec.rb
@@ -1,13 +1,6 @@
 feature 'Hit Points' do
   scenario 'shows Player 2 HP' do
-    visit('/')
-    fill_in :player_1_name, with: 'Emma'
-    fill_in :player_2_name, with: 'Jonatan'
-    click_button ("Submit")
+    sign_in_and_play
     expect(page).to have_content 'Jonatan: 100HP'
   end
 end
-
-# As Player 1,
-# So I can see how close I am to winning
-# I want to see Player 2's Hit Points
